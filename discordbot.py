675b01657f3ed78f550f487ed6620732da1e0d6a
aikawa1936 <77907300+aikawa1936@users.noreply.github.com>
2021-01-25 13:41:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,23 +1,19 @@
+import discord
 from discord.ext import commands
-import os
+import asyncio
 import traceback
-
-bot = commands.Bot(command_prefix='/')
+import random
+import os
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
+client = discord.Client()
+
+@client.event
 async def on_message(message):
-    try:
-        if message.author.bot:
-            return
-        await bot.process_commands(message)
-    except Exception:
-        await message.channel.send(f'```\n{traceback.format_exc()}\n```')
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
+    if message.author.bot:
+        return
+    if message.content == "/hello":
+        msg = message.author.mention + "さん、おはようございます！今日もよろしくお願いします。"
+    await client.send_message(message.channel, msg)
 
-@bot.command()
-async def neko(ctx):
-    await ctx.send('にゃーん')
-
-bot.run(token)
+client.run(token)
