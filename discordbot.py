af4b7da90d90f7c114614bf1b9a6afc6559f1f39
aikawa1936 <77907300+aikawa1936@users.noreply.github.com>
2021-01-25 09:46:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,7 +8,7 @@ TOKEN = os.environ['DISCORD_BOT_TOKEN']
 @bot.event
 async def on_message(message):
     try:
-        if message.author.bot:    # メッセージ送信者がBotだった場合は無視する
+        if message.author.bot:
             return
         await bot.process_commands(message)
     except Exception:
