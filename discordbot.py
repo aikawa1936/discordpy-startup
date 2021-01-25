c152a84171e741aa2c8401079aee668a4805aa4b
aikawa1936 <77907300+aikawa1936@users.noreply.github.com>
2021-01-25 19:01:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,9 +9,6 @@ token = os.environ.get('DISCORD_BOT_TOKEN')
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-# リスト置き場
-neko = ['にゃーん♪', 'にゃにゃーん！', 'にゃぅ？', 'にゃぁ……']
-
 # 起動時に動作する処理
 @client.event
 async def on_ready():
@@ -26,7 +23,7 @@ async def on_message(message):
         return
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
-        await message.channel.send(random.choice(neko))
+        await message.channel.send('にゃーん♪')
 
 # Botの起動とDiscordサーバーへの接続
 client.run(token)
