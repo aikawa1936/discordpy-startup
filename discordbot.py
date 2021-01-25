f09f95bc397015d21982a9047ce3f2723f84b66e
aikawa1936 <77907300+aikawa1936@users.noreply.github.com>
2021-01-25 19:14:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,7 +22,7 @@ async def on_message(message):
         return
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
-        await message.channel.send('にゃーん♪')
+        await message.channel.send('ばかー！')
 
 # Botの起動とDiscordサーバーへの接続
 client.run(token)
