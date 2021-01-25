8c975cf86a1df6a181d51278cf34f02b7105b31b
aikawa1936 <77907300+aikawa1936@users.noreply.github.com>
2021-01-25 10:05:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,4 +5,9 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@bot.command()
+async def neko(ctx):
+    await ctx.send('にゃーん')
+
+
 bot.run(token)
